6429874e7049a3145c74ffbb74a57b4c554dba46
Mateus da silva pereira <mateusdev.cs@gmail.com>
2021-10-24 18:06:55 -0300

    Exercicio OO, do curso de C#

---
+++ ExeUdemy/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ExeUdemy
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ ExeUdemy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExeUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee one = new Employee();
+            Employee two = new Employee();
+
+            Console.WriteLine("First employee data");
+            Console.Write("Name : ");
+            one.Name = Console.ReadLine();
+            Console.Write("Salary of Employee : ");
+            one.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Second employee data");
+            Console.Write("Name : ");
+            two.Name = Console.ReadLine();
+            Console.Write("Salary of employee : ");
+            two.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+
+            double average = (one.Salary + two.Salary) / 2.0;
+
+            Console.WriteLine($"Average salary : {average.ToString("F2", CultureInfo.InvariantCulture)} ");  
+        }
+    }
+}
